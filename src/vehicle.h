7ef3bc65702373d5efb810a77f40e5e606a412eb
ArghyaBan2006 <arghyab.com@gmail.com>
2026-03-29 22:38:20 +0530

    Update vehicle.h

--- src/vehicle.h
+++ src/vehicle.h
@@ -1,8 +1,26 @@
-#include "Vehicle.h"
+#ifndef VEHICLE_H
+#define VEHICLE_H
 
-class Bike : public Vehicle {
+#include <iostream>
+using namespace std;
+
+class Vehicle {
+protected:
+    string name;
+    int price;
+
 public:
-    void type() {
-        cout << "Type: Bike" << endl;
+    void setData(string n, int p) {
+        name = n;
+        price = p;
+    }
+
+    int getPrice() {
+        return price;
+    }
+
+    virtual void display() {
+        cout << "Vehicle: " << name << endl;
+        cout << "Price per day: " << price << endl;
     }
 };
